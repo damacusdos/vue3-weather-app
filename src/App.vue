49b4c30520fba3ffabc761957941e048582e0e86
Alison Huang <alisonhcw@gmail.com>
2023-01-04 12:25:20 +0800

    project init and tailwind basic setup

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <SiteNavigation />
+    <RouterView></RouterView>
+  </div>
+</template>
+
+<script setup>
+import SiteNavigation from "./components/SiteNavigation.vue";
+import { RouterView } from "vue-router";
+</script>
+
+<style lang="scss" scoped></style>
